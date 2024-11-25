f0c2c975ba74f09fc3aaf77eb590ee830fa0c37f
juliataranczuk <juliataranczuk@gmail.com>
2024-11-25 13:42:13 +0100

    chore: add new users

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ users: list = [
     {'name': 'Dominik', 'posts': 3, 'city': 'Poznań'},
     {'name': 'Szymon z wąsem', 'posts': 5, 'city': 'Toruń'},
     {'name': 'Szymon', 'posts': 7, 'city': 'Szczecin'},
-    {'name': 'Patryk', 'posts': 9, 'city': 'Łódź'},
+    {'name': 'Patrycja', 'posts': 9, 'city': 'Warszawa'},
+    {'name': 'Patrycja', 'posts': 3, 'city': 'Łódź'},
+    {'name': 'Amelia', 'posts': 11, 'city': 'Toruń'},
+    {'name': 'Kinga', 'posts': 4, 'city': 'Iława'},
+    {'name': 'Dominik', 'posts': 2, 'city': 'Szczecin'},
 
+
+]
 #TODO please update user list
 
 print(f'Witaj {users[0]['name']}!!')

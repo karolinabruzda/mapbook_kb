0874ba1bb1f2f67ad9bd2e95f075f1fcb4b561f1
karolinabruzda <karolina.bruzda@student.wat.edu.pl>
2024-12-02 14:33:42 +0100

    feat: add remove user function

--- mapbook/crud.py
+++ mapbook/crud.py
@@ -12,3 +12,11 @@ def add_user(userlist:list)->None:
     new_city:str=input('podaj miasto ')
     new_user: dict = {'name': new_name, 'posts': new_posts, 'city': new_city}
     userlist.append(new_user)
+
+
+def remove_user(userlist:list)->None:
+    user_to_find:str = input('podaj imie osoby do usunięcia: ')
+    for user in userlist:
+        if user['name'] == user_to_find:
+            print(f'usuwam: {user}')
+            userlist.remove(user)
